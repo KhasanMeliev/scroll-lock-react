193ea29235dabe846d6b89bd8940e331f6748ed6
KhasanMeliev <khasanmeliev2@gmail.com>
2022-09-24 17:27:52 +0500

    added button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import { useScrollLock } from "@mantine/hooks";
-import { Group, Button } from "@mantine/core";
+import { Group, Button, Anchor } from "@mantine/core";
 import { AiFillLock, AiFillUnlock } from "react-icons/ai";
 
 function App() {
   const [scrollLocked, setScrollLocked] = useScrollLock();
   return (
-    <Group>
+    <Group style={{ display: "flex", flexDirection: "column", gap: "50px" }}>
       <Button
         onClick={() => setScrollLocked((c) => !c)}
         variant="outline"
@@ -17,6 +17,12 @@ function App() {
       >
         {scrollLocked ? "Unlock scroll" : "Lock scroll"}
       </Button>
+      <Anchor
+        href="https://www.youtube.com/watch?v=JOffqSrMlrg"
+        target="_blank"
+      >
+        <Button size="lg">My Coding Channel</Button>
+      </Anchor>
     </Group>
   );
 }
